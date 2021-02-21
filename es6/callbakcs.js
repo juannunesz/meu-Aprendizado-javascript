e8f698e039e83b8008ed78d43079edba6170872d
juannunesz <juannuneszbr@gmail.com>
2021-02-21 20:28:18 -0300

    add conteudo de callbacks

--- es6/callbakcs.js
+++ es6/callbakcs.js
@@ -1,7 +1,39 @@
-const $button = document.querySelector('button')
+function newButton(text){
 
-const handlerClick = () => {
-  console.log('Botão foi clicado')
+  const $body = document.querySelector('body');
+  
+  const $button = document.createElement('button');
+  
+  $button.textContent = text;
+  
+  $body.insertAdjacentElement('beforeend',$button);
+
+  return $button
 }
 
-$button.addEventListener('click',handlerClick);
+const login = newButton('Login')
+const singUp = newButton('Sing Up')
+
+console.log(login)
+console.log(singUp)
+
+login.style.cssText = `
+  color: red;
+`
+
+login.addEventListener('click', () => {
+  console.log('Fazendo login');
+})
+
+singUp.style.cssText = `
+  color: green;
+  text-transform: uppercase;
+  background: black;
+`
+
+singUp.addEventListener('click', () => {
+  console.log('Fazendo login');
+})
+
+
+
